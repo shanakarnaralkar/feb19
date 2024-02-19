131ec2df93424de5e93fdde05edc1b525eaceb5a
shankar <shankarnaralkar15@gmail.com>
2024-02-19 14:19:19 +0530

    new chanes

---
+++ src/test/java/com/example/demo/CurdOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
